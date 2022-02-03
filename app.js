2722a22aee179370d66b1a9d50d945ad296daa5e
FishDentistry <cfronk12@gmail.com>
2022-02-02 19:26:12 -0500

    Updated homepage

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'MongoDB Connect Info Here";
+var mongoDB = 'Database Address Here';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
